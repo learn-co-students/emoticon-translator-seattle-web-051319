2ffcb1a928589136dc4543dc25606ddaaec80ad8
fut0k0 <cccabell@icloud.com>
2019-04-28 15:23:15 -0700

    Passing lab

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,13 +1,40 @@
-# require modules here
+require "yaml"
 
-def load_library
-  # code goes here
+def load_library(path)
+	#create lookup table structure
+	table = {}
+	table["get_meaning"] = {}
+	table["get_emoticon"] = {}
+	
+	#populate lookup table
+	YAML.load_file(path).each do |emotion, symbols|
+		table["get_meaning"][symbols[1]] = emotion
+		table["get_emoticon"][symbols[0]] = symbols[1]
+	end	
+	
+	table
 end
 
-def get_japanese_emoticon
-  # code goes here
+def get_japanese_emoticon(path, emoticon)
+	#load table
+	table = load_library(path)
+	
+	#look up english emoticon and return result
+	if table["get_emoticon"].keys.include?(emoticon)
+		return table["get_emoticon"][emoticon]
+	else
+		return "Sorry, that emoticon was not found"	
+	end
 end
 
-def get_english_meaning
-  # code goes here
+def get_english_meaning(path, emoticon)
+	#load table
+	table = load_library(path)
+	
+	#look up japanese emoticon and return result
+	if table["get_meaning"].keys.include?(emoticon)
+		return table["get_meaning"][emoticon]
+	else
+		return "Sorry, that emoticon was not found"
+	end	
 end
